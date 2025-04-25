d7b2586ca99ee08bc37b0b39d3d283e7ef695c74
JovannMC <jovannmc@femboyfurry.net>
2025-04-25 21:40:40 +0300

    feat: send sensor, rotation, & accel data w/ tests
    many things
    
    waow it works so far

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 use firmware_protocol::deku::prelude::*;
-use firmware_protocol::{BoardType, ImuType, McuType, Packet, SbPacket};
+use firmware_protocol::{
+    BoardType, ImuType, McuType, Packet, SbPacket, SensorDataType, SensorStatus, SlimeQuaternion,
+};
 use std::sync::{Arc, Mutex};
 use tokio::net::UdpSocket;
 use tokio::sync::watch;
+use rand::Rng;
 
 pub struct TrackerState {
     pub status: String,
-    pub packet_number: u32,
+    pub packet_number: u64,
     pub last_received_packet_time: u16,
 }
 
@@ -17,17 +20,25 @@ pub enum FirmwareFeatureFlags {
     None = 9999,
 }
 
+#[derive(Debug)]
+pub struct Sensor {
+    pub sensor_id: u8,
+    pub sensor_type: ImuType,
+    pub sensor_status: SensorStatus,
+}
+
 pub struct EmulatedTracker {
     // Configuration
     mac_address: [u8; 6],
     firmware_version: String,
     feature_flags: FirmwareFeatureFlags,
     board_type: BoardType,
-    imu_type: ImuType,
     mcu_type: McuType,
     server_timeout: u64,
     server_ip: String,
     server_port: u16,
+
+    sensors: Vec<Sensor>,
 
     // Socket stuff
     state: Arc<Mutex<TrackerState>>,
@@ -42,7 +53,6 @@ impl EmulatedTracker {
         firmware_version: String,
         feature_flags: Option<FirmwareFeatureFlags>,
         board_type: Option<BoardType>,
-        imu_type: Option<ImuType>,
         mcu_type: Option<McuType>,
         server_ip: Option<String>,
         server_discovery_port: Option<u16>,
@@ -51,7 +61,6 @@ impl EmulatedTracker {
         // Set default values if the parameters are None
         let feature_flags = feature_flags.unwrap_or(FirmwareFeatureFlags::None);
         let board_type = board_type.unwrap_or(BoardType::Unknown(0));
-        let imu_type = imu_type.unwrap_or(ImuType::Unknown(0));
         let mcu_type = mcu_type.unwrap_or(McuType::Unknown(0));
         let server_ip = server_ip.unwrap_or("255.255.255.255".to_string());
         let server_port = server_discovery_port.unwrap_or(6969);
@@ -70,8 +79,8 @@ impl EmulatedTracker {
             firmware_version,
             feature_flags,
             board_type,
-            imu_type,
             mcu_type,
+            sensors: Vec::new(),
             server_timeout,
             server_ip,
             server_port,
@@ -82,63 +91,70 @@ impl EmulatedTracker {
         })
     }
 
+    /*
+     * Server init functions
+     */
+
     pub async fn init(&mut self) -> Result<(), String> {
         let mut state = self.state.lock().unwrap();
         if state.status != "initializing" {
             return Ok(());
         }
-    
-        // Create a socket and bind to the local address
-        let bind_address = format!("{}:{}", "0.0.0.0", 0); // Bind to all interfaces on a random port
+
+        let bind_address = format!("{}:{}", "0.0.0.0", 0);
         let socket = UdpSocket::bind(&bind_address)
             .await
             .map_err(|e| format!("Failed to bind socket: {}", e))?;
-    
-        // Set the broadcast option to true
-        socket.set_broadcast(true).map_err(|e| format!("Failed to set broadcast option: {}", e))?;
-    
+
+        socket
+            .set_broadcast(true)
+            .map_err(|e| format!("Failed to set broadcast option: {}", e))?;
+
         self.socket = Some(Arc::new(socket));
-    
+
         self.status_tx.send("idle".to_string()).unwrap();
         state.status = "idle".to_string();
         drop(state);
-    
-        let server_ip = self.server_ip.clone();
-        let server_port = self.server_port;
-    
+
         let mut discovery_interval = tokio::time::interval(std::time::Duration::from_secs(1));
         let mut status_rx = self.status_rx.clone();
-    
+
         loop {
             tokio::select! {
                 _ = discovery_interval.tick() => {
-                    // Send handshake if not connected
                     let state = self.state.lock().unwrap();
                     if state.status != "connected-to-server" {
                         drop(state);
-                        self.send_handshake(server_ip.clone(), server_port).await?;
+                        self.send_handshake().await?;
                     } else {
                         break;
                     }
                 }
-                result = status_rx.changed() => {
-                    match result {
-                        Ok(_) => {
-                            let state = self.state.lock().unwrap();
-                            println!("Status changed to: {}", state.status);
-                            drop(state);
-                        }
-                        Err(e) => {
-                            println!("Error receiving status update: {}", e);
-                            break;
+
+                _ = async {
+                    if let Some(socket) = self.socket.as_ref() {
+                        let mut buf = [0u8; 1024];
+                        match socket.recv_from(&mut buf).await {
+                            Ok((size, addr)) => {
+                                println!("Received data from: {:?}, size: {}", addr, size);
+                                // Process the received data here
+                                let mut state = self.state.lock().unwrap();
+                                state.status = "connected-to-server".to_string();
+                                self.status_tx.send("connected-to-server".to_string()).unwrap();
+                                drop(state);
+                            }
+                            Err(e) => {
+                                println!("Failed to receive data: {}", e);
+                            }
                         }
                     }
-                }
+                } => {}
             }
         }
-    
+
         Ok(())
     }
+
     pub async fn deinit(&mut self) -> Result<(), String> {
         let mut state = self.state.lock().unwrap();
         if state.status == "initializing" {
@@ -146,26 +162,121 @@ impl EmulatedTracker {
         }
 
         self.socket = None;
-        self.status_tx.send("initializing".to_string()).unwrap(); // Update status
+        self.status_tx.send("initializing".to_string()).unwrap();
         state.status = "initializing".to_string();
         drop(state);
         Ok(())
     }
 
-    //fn handle_packet(&self, packet: &[u8]) {}
+    /*
+     * Tracker functions
+     */
 
-    fn increment_packet_number(&self) {
-        let mut state = self.state.lock().unwrap();
-        state.packet_number += 1;
+    pub async fn add_sensor(
+        &mut self,
+        sensor_type: ImuType,
+        sensor_status: SensorStatus,
+    ) -> Result<(), String> {
+        let sensor_id = self.sensors.len() as u8;
+        let sensor = Sensor {
+            sensor_id,
+            sensor_type,
+            sensor_status,
+        };
+        self.send_sensor_info(&sensor).await?;
+        self.sensors.push(sensor);
+        Ok(())
     }
 
-    async fn send_handshake(&self, server_ip: String, server_port: u16) -> Result<(), String> {
+    /*
+     * Packet sending functions
+     */
+
+    // send_battery_level, send_rotation_data, send_acceleration, send_temperature, send_magnetometer_accuracy, send_signal_strength, send_user_action
+    pub async fn send_sensor_info(&self, sensor: &Sensor) -> Result<(), String> {
+        let data = SbPacket::SensorInfo {
+            sensor_id: sensor.sensor_id,
+            sensor_type: self.clone_sensor_type(&sensor.sensor_type),
+            sensor_status: self.clone_sensor_status(&sensor.sensor_status),
+        };
+
+        let packet_number = {
+            let state = self.state.lock().unwrap();
+            state.packet_number
+        };
+        let packet = Packet::new(packet_number, data);
+
+        self.send_packet(packet).await
+    }
+
+    pub async fn send_rotation_data(
+        &self,
+        sensor_id: u8,
+        data_type: SensorDataType,
+        rotation_data: SlimeQuaternion,
+        accuracy: u8,
+    ) -> Result<(), String> {
+        let data = SbPacket::RotationData {
+            sensor_id,
+            data_type,
+            quat: rotation_data,
+            calibration_info: accuracy,
+        };
+
+        let packet_number = {
+            let state = self.state.lock().unwrap();
+            state.packet_number
+        };
+        let packet = Packet::new(packet_number, data);
+
+        self.send_packet(packet).await
+    }
+
+    pub async fn send_acceleration(
+        &self,
+        sensor_id: u8,
+        acceleration: (f32, f32, f32),
+    ) -> Result<(), String> {
+        let data = SbPacket::Acceleration {
+            sensor_id,
+            vector: acceleration,
+        };
+
+        let packet_number = {
+            let state = self.state.lock().unwrap();
+            state.packet_number
+        };
+        let packet = Packet::new(packet_number, data);
+
+        self.send_packet(packet).await
+    }
+
+    /*
+     * Packet sending utilities
+     */
+
+    async fn send_packet(&self, packet: Packet<SbPacket>) -> Result<(), String> {
+        println!("Sending packet: {:?}", packet);
+
+        let socket = self.socket.as_ref().expect("Socket not initialized");
+        socket
+            .send_to(
+                &packet.to_bytes().unwrap(),
+                (self.server_ip.clone(), self.server_port),
+            )
+            .await
+            .map_err(|e| e.to_string())?;
+
+        Ok(())
+    }
+
+    async fn send_handshake(&self) -> Result<(), String> {
         let data = SbPacket::Handshake {
             board: self.clone_board_type(),
-            imu: self.clone_imu_type(),
+            imu: self.clone_sensor_type(&ImuType::Unknown(0)),
             mcu: self.clone_mcu_type(),
             imu_info: (0, 0, 0),
-            build: 1,
+            build: 13, // current version is 13 apparently
             firmware: self.firmware_version.clone().into(),
             mac_address: self.mac_address,
         };
@@ -173,7 +284,10 @@ impl EmulatedTracker {
 
         let socket = self.socket.as_ref().ok_or("Socket not initialized")?;
         socket
-            .send_to(&packet.to_bytes().unwrap(), (server_ip, server_port))
+            .send_to(
+                &packet.to_bytes().unwrap(),
+                (self.server_ip.clone(), self.server_port),
+            )
             .await
             .map_err(|e| e.to_string())?;
         Ok(())
@@ -208,32 +322,6 @@ impl EmulatedTracker {
         }
     }
 
-    fn clone_imu_type(&self) -> ImuType {
-        match &self.imu_type {
-            ImuType::Mpu9250 => firmware_protocol::ImuType::Mpu9250,
-            ImuType::Mpu6500 => firmware_protocol::ImuType::Mpu6500,
-            ImuType::Bno080 => firmware_protocol::ImuType::Bno080,
-            ImuType::Bno085 => firmware_protocol::ImuType::Bno085,
-            ImuType::Bno055 => firmware_protocol::ImuType::Bno055,
-            ImuType::Mpu6050 => firmware_protocol::ImuType::Mpu6050,
-            ImuType::Bno086 => firmware_protocol::ImuType::Bno086,
-            ImuType::Bmi160 => firmware_protocol::ImuType::Bmi160,
-            ImuType::Icm20948 => firmware_protocol::ImuType::Icm20948,
-            ImuType::Icm42688 => firmware_protocol::ImuType::Icm42688,
-            ImuType::Bmi270 => firmware_protocol::ImuType::Bmi270,
-            ImuType::Lsm6ds3trc => firmware_protocol::ImuType::Lsm6ds3trc,
-            ImuType::Lsm6dsv => firmware_protocol::ImuType::Lsm6dsv,
-            ImuType::Lsm6dso => firmware_protocol::ImuType::Lsm6dso,
-            ImuType::Lsm6dsr => firmware_protocol::ImuType::Lsm6dsr,
-            ImuType::Icm45686 => firmware_protocol::ImuType::Icm45686,
-            ImuType::Icm45605 => firmware_protocol::ImuType::Icm45605,
-            ImuType::AdcResistance => firmware_protocol::ImuType::AdcResistance,
-            ImuType::DevReserved => firmware_protocol::ImuType::DevReserved,
-            ImuType::Unknown(val) => firmware_protocol::ImuType::Unknown(*val),
-            _ => firmware_protocol::ImuType::Unknown(0),
-        }
-    }
-
     fn clone_mcu_type(&self) -> McuType {
         match &self.mcu_type {
             McuType::Esp8266 => McuType::Esp8266,
@@ -249,6 +337,39 @@ impl EmulatedTracker {
             _ => McuType::Unknown(0),
         }
     }
+
+    fn clone_sensor_type(&self, imu_type: &ImuType) -> ImuType {
+        match imu_type {
+            ImuType::Mpu9250 => ImuType::Mpu9250,
+            ImuType::Mpu6500 => ImuType::Mpu6500,
+            ImuType::Bno080 => ImuType::Bno080,
+            ImuType::Bno085 => ImuType::Bno085,
+            ImuType::Bno055 => ImuType::Bno055,
+            ImuType::Mpu6050 => ImuType::Mpu6050,
+            ImuType::Bno086 => ImuType::Bno086,
+            ImuType::Bmi160 => ImuType::Bmi160,
+            ImuType::Icm20948 => ImuType::Icm20948,
+            ImuType::Icm42688 => ImuType::Icm42688,
+            ImuType::Bmi270 => ImuType::Bmi270,
+            ImuType::Lsm6ds3trc => ImuType::Lsm6ds3trc,
+            ImuType::Lsm6dsv => ImuType::Lsm6dsv,
+            ImuType::Lsm6dso => ImuType::Lsm6dso,
+            ImuType::Lsm6dsr => ImuType::Lsm6dsr,
+            ImuType::Icm45686 => ImuType::Icm45686,
+            ImuType::Icm45605 => ImuType::Icm45605,
+            ImuType::AdcResistance => ImuType::AdcResistance,
+            ImuType::DevReserved => ImuType::DevReserved,
+            ImuType::Unknown(val) => ImuType::Unknown(*val),
+            _ => ImuType::Unknown(0),
+        }
+    }
+
+    fn clone_sensor_status(&self, status: &SensorStatus) -> SensorStatus {
+        match status {
+            SensorStatus::Ok => SensorStatus::Ok,
+            SensorStatus::Offline => SensorStatus::Offline,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -257,21 +378,19 @@ mod tests {
     use tokio::time::Duration;
 
     #[tokio::test]
-    async fn test_send_handshake() {
+    async fn test_send_sensor_data() {
+        // tracker setup
         let mac_address = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06];
         let firmware_version = "test_firmware".to_string();
-        let server_discovery_ip = "255.255.255.255".to_string();
-        let server_discovery_port = 6969;
 
         let mut tracker = EmulatedTracker::new(
             mac_address,
-            firmware_version.clone(),
+            firmware_version,
             None,
             None,
             None,
             None,
-            Some(server_discovery_ip.clone()),
-            Some(server_discovery_port),
+            None,
             None,
         )
         .await
@@ -279,10 +398,47 @@ mod tests {
 
         tracker.init().await.expect("Failed to initialize tracker");
 
+        // add sensor
         tracker
-            .send_handshake(server_discovery_ip.clone(), server_discovery_port)
+            .add_sensor(ImuType::Mpu9250, SensorStatus::Ok)
             .await
-            .expect("Failed to send handshake");
+            .expect("Failed to add sensor");
+
+        // random sensor data (rotation/accel)
+        for _ in 0..5 {
+            let quat = SlimeQuaternion {
+                i: rand::random::<f32>() * 2.0 - 1.0,
+                j: rand::random::<f32>() * 2.0 - 1.0,
+                k: rand::random::<f32>() * 2.0 - 1.0,
+                w: rand::random::<f32>() * 2.0 - 1.0,
+            };
+
+            tracker
+                .send_rotation_data(
+                    0,
+                    SensorDataType::Normal,
+                    quat,
+                    100, // idk the range of accuracy it should be
+                )
+                .await
+                .expect("Failed to send rotation data");
+
+            let accel = (
+                rand::random::<f32>() * 20.0 - 10.0,
+                rand::random::<f32>() * 20.0 - 10.0,
+                rand::random::<f32>() * 20.0 - 10.0,
+            );
+
+            tracker
+                .send_acceleration(
+                    0,
+                    accel,
+                )
+                .await
+                .expect("Failed to send acceleration data");
+
+            tokio::time::sleep(Duration::from_millis(10)).await;
+        }
 
         tokio::time::sleep(Duration::from_millis(100)).await;
     }
